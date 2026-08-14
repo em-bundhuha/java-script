00ee28e77a55e6fcbba94d35ffc2cf461ae5bca6
zarq <embundhuha19@gmail.com>
2026-08-14 12:59:22 +0800

    menambahkan else if dan sekalian dalam beberapa kondisi berbeda

--- IF Else/latihan.js
+++ IF Else/latihan.js
@@ -15,12 +15,22 @@
 // }
 
 var stok = 20;
-var stokLaku = 10;
+var stokLaku = Number(prompt("masukkan jumlah barang yang laku: "));
+var barang_cacat = Number(prompt("masukkan no barang yang cacat atau rusak: "));
+var cacat_permanen = 10;
 var no_barang = 1;
 
 for (no_barang; no_barang <= stok; no_barang++) {
-  if (no_barang <= stokLaku) {
-    console.log("barang no " + no_barang + " sudah laku");
+  if (no_barang <= stokLaku && no_barang == barang_cacat) {
+    console.log("barang no " + no_barang + " barang ini laku tapi cacat");
+  } else if (no_barang == barang_cacat) {
+    console.log("barang no " + no_barang + " cacat atau rusak");
+  } else if (no_barang === cacat_permanen && no_barang !== stokLaku) {
+    console.log(
+      "barang no " + no_barang + " cacat atau rusak permanen dan gak laku",
+    );
+  } else if (no_barang === 15 || no_barang === 3) {
+    console.log("barang no " + no_barang + " barang ini di return");
   } else {
     console.log("barang no " + no_barang + " blom laku");
   }
