4802165dd18d186556b466aff206e5b8d895b3e9
zarq <embundhuha19@gmail.com>
2026-08-22 15:54:32 +0800

    Merapikan folder dan sub folder kmd menyelesaikan materi pengulangan bersarang dan pengulangan terbalik sekalian nyoba membuat dalam bentuk ketupat tapi blom bagian bawah nya

---
+++ 1  Pengulangan/latihan/angkot1.js
@@ -0,0 +1,15 @@
+// var angkot = 1;
+// while (angkot <= 10) {
+//   console.log("Angkot NO. " + angkot + " beroprasi dengan baik");
+// angkot++;
+// }
+//  perbaikan
+console.log("penggunaan while");
+
+var jmAngkot = 20;
+var noAngkot = 1;
+while (noAngkot <= jmAngkot) {
+  console.log("angkot no " + noAngkot + " sudah berjalan");
+  noAngkot++;
+}
+
---
+++ 1  Pengulangan/while/while.js
@@ -0,0 +1,8 @@
+console.log("heloo abdur");
+
+// percobaan sebelum contah
+var Nilai_awal = 1;
+while(Nilai_awal <= 100 ) {
+  console.log('saya mencoba menerapkan ' + Nilai_awal + 'x');
+  Nilai_awal++;
+}
