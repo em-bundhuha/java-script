95e97bd1f8d0ad0591f4e8bf5af6b0daad148302
zarq <embundhuha19@gmail.com>
2026-08-29 20:46:30 +0800

    memperbaiki beberapa dan sekalian menyempurnakan

--- latihan/Tebak_angka/script.js
+++ latihan/Tebak_angka/script.js
@@ -2,21 +2,40 @@ var hasil = "";
 
 var angka = Math.floor(Math.random() * 10) + 1;
 
-var kesempatan = 3;
-for (let i = 1; i <= kesempatan; i++) {
-  var tebakan = parseInt(prompt("Tebak angka 1 - 10: "));
+var ulang = true;
+while (ulang == true) {
+  var kesempatan = 3;
+  for (let i = 1; i <= kesempatan; i++) {
+    var tebakan = parseInt(prompt("Tebak angka 1 - 10: "));
 
-  if (tebakan >= 1 && tebakan <= 10) {
     hasil = "";
-    if (tebakan == angka) {
-      alert((hasil += "tebakan anda \t" + tebakan + "\nBenar!"));
-      break;
-    } else if (tebakan < angka) {
-      alert((hasil += "tebakan anda \t" + tebakan + "\nterlalu rendah\n"));
+    if (tebakan >= 1 && tebakan <= 10) {
+      if (tebakan == angka) {
+        alert((hasil += "tebakan anda \t" + tebakan + "\nBenar!"));
+        break;
+      } else if (tebakan < angka) {
+        alert((hasil += "tebakan anda \t" + tebakan + "\nterlalu rendah\n"));
+      } else {
+        alert((hasil += "tebakan anda \t" + tebakan + "\nterlalu tinggi"));
+      }
     } else {
-      alert((hasil += "tebakan anda \t" + tebakan + "\nterlalu tinggi"));
+      sisa = 3 - i;
+      if (sisa == 0) {
+        alert(
+          (hasil +=
+            "angka yang anda masukkan tidak termasuk dari 1-10\ngoblog minim literasi gitu aja gak paham "),
+        );
+      } else {
+        alert(
+          (hasil +=
+            "angka yang anda masukkan tidak termasuk dari 1-10\nsisa " +
+            sisa +
+            " kesempatan"),
+        );
+      }
     }
-  } else {
-    alert((hasil += "angka yang anda masukkan tidak termasuk dari 1-10"));
   }
+  ulang = confirm("lagi ?");
 }
+
+alert('terimakasih udah mau bermain')
